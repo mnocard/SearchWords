cc43913977c272950b642d6fc2c504a489a73f8c
Хайрбек Мамбетов <mnocard@gmail.com>
2024-02-17 16:56:57 +0500

    simple route

--- api/index.js
+++ api/index.js
@@ -11,4 +11,8 @@ mongoose.connect(process.env.MONGO)
 const app = express();
 app.listen(3000, () => {
     console.log("port 3000!");
-});
+}); 
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Api is working!'});
+}); 
