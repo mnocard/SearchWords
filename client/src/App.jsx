d0b61b83b89ce8924ab98a606239d070d0c8c706
Хайрбек Мамбетов <mnocard@gmail.com>
2024-03-09 07:41:31 +0500

    profile pucture

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,20 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/SignIn' element={<SignIn />} />
+        <Route path='/SignUp' element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/Profile' element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+    const { currentUser } = useSelector(state => state.user);
     return (
         <div className='bg-cyan-600'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -10,8 +12,16 @@ export default function Header() {
                 <ul className='flex gap-4'>
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/About'>About</Link></li>
-                    <li><Link to='/SignUp'>Sign Up</Link></li>
-                    <li><Link to='/SignIn'>Sign In</Link></li>
+                    {currentUser
+                        ? (<li>
+                            <Link to='/Profile'><img
+                                src={currentUser.profilePicture}
+                                alt="Profile"
+                                className="h-7 w-7 rounded-full object-cover" />
+                            </Link>
+                        </li>)
+                        : (<li><Link to='/SignIn'>Sign In</Link></li>)
+                    }
                 </ul>
             </div>
         </div>
